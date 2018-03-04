c0830614a693a488aabdc852faed7671512a834d
sheroziy <sheroziy_uz@yahoo.com>
2018-03-04 16:56:56 -0500

    added print statement

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -3,7 +3,8 @@ package test;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Class");
+		System.out.println("Hello Team");
+		System.out.println("Today Sunny day");
 
 	}
 
