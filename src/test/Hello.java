9985a0ca8eb7775302a05b22857823f7ed800562
sheroziy <sheroziy_uz@yahoo.com>
2018-03-04 17:12:24 -0500

    new statement

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Team");
 		System.out.println("Today Sunny day");
+		System.out.println("Today Sunny day and Bright");
 
 	}
 
